cb532051edbc0ed5b3dc18c8d8c06540f8dbff6d
randyliu4345 <randy.liu08@gmail.com>
2020-07-30 11:35:07 -0700

    aaa

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     }
 
     callAPI() {
-        fetch("http://localhost:9000/testAPI")
+        fetch("http://localhost:8080/users/query_param")
             .then(res => res.text())
             .then(res => this.setState({ apiResponse: res }))
             .catch(err => err);
